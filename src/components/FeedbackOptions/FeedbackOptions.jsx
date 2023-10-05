f099709b83679f4b23af07c13d494c954de0ad09
unknown <rubikonspace@ukr.net>
2023-10-05 15:20:37 +0300

    s

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,7 +2,7 @@ import { DivButtonContainer } from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ onLeaveFeedback }) => {
   return (
-    // вса
+    // df
     <DivButtonContainer>
       <button onClick={() => onLeaveFeedback('good')}>Good</button>
       <button onClick={() => onLeaveFeedback('neutral')}>Neutral</button>
